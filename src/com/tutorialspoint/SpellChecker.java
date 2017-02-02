915f5bc6c403d490ec2e6dbe9b9f7d667814c4c1
josecruz <jose.m.cruz.perez@accenture.com>
2017-02-02 09:40:20 -0600

    Injecting inner beans

---
+++ src/com/tutorialspoint/SpellChecker.java
@@ -0,0 +1,15 @@
+package com.tutorialspoint;
+
+/**
+ * Created by jose.m.cruz.perez on 31/01/2017.
+ */
+public class SpellChecker {
+    public SpellChecker(){
+        System.out.println("Inside SpellChecker constructor." );
+    }
+
+    public void checkSpelling(){
+        System.out.println("Inside checkSpelling." );
+    }
+
+}
