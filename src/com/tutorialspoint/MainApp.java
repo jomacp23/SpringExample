ba5cc243f544f65e493b430f5a35282708bb37d4
josecruz <jose.m.cruz.perez@accenture.com>
2017-02-07 10:38:56 -0600

    Injecting Collections Example

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -8,15 +8,11 @@ public class MainApp {
    public static void main(String[] args) {
       ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+      JavaCollection jc=(JavaCollection)context.getBean("javaCollection");
 
-      objA.getMessage1();
-      objA.getMessage2();
-
-      HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
-      objB.getMessage1();
-      objB.getMessage2();
-      objB.getMessage3();
+      jc.getAddressList();
+      jc.getAddressSet();
+      jc.getAddressMap();
+      jc.getAddressProp();
    }
-   
 }
