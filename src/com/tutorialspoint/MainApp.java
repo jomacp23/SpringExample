04cc77aa4bce0a99557379a4288db0a58d09ab23
josecruz <jose.m.cruz.perez@accenture.com>
2017-02-14 11:41:42 -0600

    AOP example

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -8,15 +8,12 @@ public class MainApp {
    public static void main(String[] args) {
       ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+      Student student = (Student) context.getBean("student");
 
-      objA.getMessage1();
-      objA.getMessage2();
-
-      HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
-      objB.getMessage1();
-      objB.getMessage2();
-      objB.getMessage3();
+      student.getName();
+      student.getAge();
+      
+      student.printThrowException();
    }
    
 }
